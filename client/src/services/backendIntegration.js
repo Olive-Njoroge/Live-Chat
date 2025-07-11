f335ee50e3c8751c43c9fdea7d3a786e6cdbddfb
Olive Njoroge <njorogeolive001@gmail.com>
2025-07-11 09:25:11 +0300

    mongoatlas

--- client/src/services/backendIntegration.js
+++ client/src/services/backendIntegration.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import io from 'socket.io-client'
 
-const BackendBaseUrl = "http://localhost:5000";
-const APIBaseUrl = "http://localhost:5000/api"
+const BackendBaseUrl = "https://live-chat-3-w90f.onrender.com";
+const APIBaseUrl = "https://live-chat-3-w90f.onrender.com/api"
 
 const API = axios.create({
     baseURL : APIBaseUrl
